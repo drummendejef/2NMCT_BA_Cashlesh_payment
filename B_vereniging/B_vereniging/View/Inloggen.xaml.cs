363359d3a7851895a7054c6c4aa5cb724e377b2e
Joren <jorenvandamme@hotmail.com>
2014-10-10 16:50:30 +0200

    Views aangepast
    De view helemaal herwerkt.

--- B_vereniging/B_vereniging/View/Inloggen.xaml.cs
+++ B_vereniging/B_vereniging/View/Inloggen.xaml.cs
@@ -10,19 +10,28 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace B_vereniging.View
 {
     /// <summary>
-    /// Interaction logic for Page1.xaml
+    /// Interaction logic for Inloggen.xaml
     /// </summary>
-    public partial class Page1 : Page
+    public partial class Inloggen : Window
     {
-        public Page1()
+        public Inloggen()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //Eerste scherm verbergen
+            this.Hide();
+
+            //Nieuw scherm aanmaken en oproepen
+            var mainscreen = new MainWindow();
+            mainscreen.Show();
         }
     }
 }
